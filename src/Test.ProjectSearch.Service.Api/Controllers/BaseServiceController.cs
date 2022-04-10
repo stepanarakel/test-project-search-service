3f1b6c720be9f3e95b1d25fc283a37fe8708f813
Arakelyanc Stepan <arakeliantssv@monqlab.com>
2022-04-10 20:22:18 +0300

    🔨 добавлены константы приложения. 🔨 Добавлен базовый контроллер.

---
+++ src/Test.ProjectSearch.Service.Api/Controllers/BaseServiceController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+/// <summary>
+/// Базовый контроллер сервиса.
+/// </summary>
+[Area("api/test-project-search")]
+[ApiController]
+public class BaseServiceController : ControllerBase { }
